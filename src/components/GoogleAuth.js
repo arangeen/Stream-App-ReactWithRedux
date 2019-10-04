399211b37a62045f2872182c9ecfde5932112bbb
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-10-04 16:58:19 -0700

    Wiring up Google API library

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "551469314953-mie8fdbdvmnn1em59ldljl7e5m43lc70.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
