18db246db458225f50a0322aee5e05f435e34211
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-10-05 13:37:22 -0700

    Redux setup

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+// will have all action creators inside of it
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+// just to have a reducer for now so app can run
+export default combineReducers({
+  replaceMe: () => "gfgrg"
+});
