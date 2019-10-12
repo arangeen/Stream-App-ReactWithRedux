fe5f83ddd5d57937a59ec6c3022e314dbb2702cb
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-10-12 16:28:21 -0700

    Rendering all Streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -6,12 +6,38 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>StreamList</div>;
+    //console.log(this.props.streams)
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
+// we need to get list of streams available as props inside our component
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchStreams }
 )(StreamList);
