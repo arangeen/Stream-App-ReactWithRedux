b5d4349c12e171cb68de01033a1a3407e53096a6
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-10-11 17:03:11 -0700

    Rest of the action creators created using REST-ful conventions

---
+++ src/actions/types.js
@@ -0,0 +1,6 @@
+//for list all records action
+export const FETCH_STREAMS = "FETCH_STREAMS";
+
+export const FETCH_STREAM = "FETCH_STREAM";
+export const DELETE_STREAM = "DELETE_STREAM";
+export const EDIT_STREAM = "EDIT_STREAM";
