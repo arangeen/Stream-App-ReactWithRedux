83db56f66b283469d16736bf05ff2d1b496404af
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-12-03 17:22:55 -0800

    StreamEdit component has id access now

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const StreamEdit = () => {
+const StreamEdit = props => {
+  // console.log(props);
   return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  //same as props inside streamEdit
+  //console.log(ownProps);
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connnect(mapStateToProps)(StreamEdit);
