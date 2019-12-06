01c43480ac5da14da0248dfdce7723c9bd6cbc29
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-12-05 17:12:13 -0800

    Fetching a stream for EditStream component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = props => {
-  // console.log(props);
-  return <div>StreamEdit</div>;
-};
+// const StreamEdit = props => {
+//   // console.log(props);
+//   return <div>StreamEdit</div>;
+// };
+
+// changing above to class based component so we
+// can use the componentDidMount() lifecyle method
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    // for class based components, must use 'this.'
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   //same as props inside streamEdit
@@ -12,4 +29,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connnect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
